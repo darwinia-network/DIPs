e5c5f5df91f431542926888b2ec4fb286464bb3d
Xavier Lau <xavier@inv.cafe>
2024-04-18 18:18:55 +0800

    Bump deps and set tab spaces to 4

--- website/conf.py
+++ website/conf.py
@@ -1,17 +1,19 @@
-project = 'dips'
-copyright = '2023, Darwinia Network'
-author = 'Darwinia Network'
+project = "dips"
+copyright = "2023, Darwinia Network"
+author = "Darwinia Network"
 
-extensions = ['myst_parser']
+extensions = ["myst_parser"]
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_title = 'Darwinia Improvement Proposals'
-html_logo = '../logo.svg'
-html_theme = 'furo'
-html_static_path = ['_static']
+html_title = "Darwinia Improvement Proposals"
+html_logo = "../logo.svg"
+html_theme = "furo"
+html_static_path = ["_static"]
+html_css_files = [
+    "custom.css",
+]
 html_theme_options = {
-    'navigation_with_keys': True,
-    'sidebar_hide_name': True,
+    "navigation_with_keys": True,
+    "sidebar_hide_name": True,
 }
-
